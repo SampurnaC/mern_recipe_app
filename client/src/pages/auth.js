b3376cb98b6f5e35cd9ded1bffaed858e59c7b86
sam <sampurna.chapagainnn@gmail.com>
2023-04-09 22:17:23 +0100

    add auth component

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,5 +1,46 @@
+import { useState } from 'react';
+
 const Auth = () => {
-  return <div>Auth</div>;
+  <div className="auth">
+    <Login />
+    <Register />
+  </div>;
 };
 
 export default Auth;
+
+const Login = () => {
+  return <div></div>;
+};
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="auth-container">
+      <form action="">
+        <h2>Register</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
